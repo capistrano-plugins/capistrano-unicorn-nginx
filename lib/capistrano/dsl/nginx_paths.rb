44024529ad7cde7f6095eef5ad9ab32cfea16115
Mike Gunderloy <MikeG1@larkfarm.com>
2015-09-10 06:41:58 -0500

    Allow customizing paths for SSL certificate and key

--- lib/capistrano/dsl/nginx_paths.rb
+++ lib/capistrano/dsl/nginx_paths.rb
@@ -27,12 +27,20 @@ module Capistrano
         "#{fetch(:nginx_server_name)}.key"
       end
 
+      def nginx_default_ssl_cert_file_path
+        "/etc/ssl/certs/"
+      end
+
+      def nginx_default_ssl_cert_key_file_path
+        "/etc/ssl/private/"
+      end
+
       def nginx_ssl_cert_file
-        "/etc/ssl/certs/#{fetch(:nginx_ssl_cert)}"
+        "#{fetch(:nginx_ssl_cert_path)}#{fetch(:nginx_ssl_cert)}"
       end
 
       def nginx_ssl_cert_key_file
-        "/etc/ssl/private/#{fetch(:nginx_ssl_cert_key)}"
+        "#{fetch(:nginx_ssl_cert_key_path)}#{fetch(:nginx_ssl_cert_key)}"
       end
 
       # log files
